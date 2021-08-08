e700151e18605230edd98beb28d275437851ec0d
yotamElm <88490320+yotamElm@users.noreply.github.com>
2021-08-08 14:50:06 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function Reset() {
+    console.log( document.getElementsByClassName('a').style)
+    document.getElementById("a").style.backgroundColor = "white"
+    document.getElementById("b").style.backgroundColor = "white"
+    document.getElementById("c").style.backgroundColor = "white"
+
+    document.getElementsByClassName('a').style.backgroundColor = "white";
+ }
